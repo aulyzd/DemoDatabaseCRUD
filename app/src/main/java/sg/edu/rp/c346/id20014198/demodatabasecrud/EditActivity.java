56827372db7a24d77b665959eee276fc8096846a
aulyzd <20014198@myrp.edu.sg>
2021-07-15 15:42:09 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20014198/demodatabasecrud/EditActivity.java
@@ -0,0 +1,67 @@
+package sg.edu.rp.c346.id20014198.demodatabasecrud;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity {
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete, btnCancel;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = findViewById(R.id.tvID);
+        etContent = findViewById(R.id.etContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnCancel = findViewById(R.id.btnCancel);
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+
+        etContent.setText(data.getNoteContent());
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+                finish();
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+            }
+        });
+
+
+    }
+
+}
